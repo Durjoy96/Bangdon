031f466a05abe72896cb9bba2fab92d08f99c1f0
Durjoy96 <mddurjoy853@gmail.com>
2024-09-23 13:21:14 +0600

    history functionality added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,7 @@ for (let btn of donateBtns) {
   btn.addEventListener("click", function (event) {
     const parentEle = event.target.parentElement.parentElement;
     getValidation(parentEle);
+    addHistory(parentEle);
   });
 };
 
@@ -17,7 +18,6 @@ function decreaseBalance(parentEle) {
   const input = parentEle.querySelector(".input");
   const balance = getElementById("balance");
   balance.innerText = getSubtractionResult(balance.innerText, input.value);
-  input.value = ""; //reset the input value
 };
 
 function getValidation(parentEle) {
@@ -31,6 +31,23 @@ function getValidation(parentEle) {
     decreaseBalance(parentEle); //decrease balance
     getElementById("modal").showModal(); //show the modal
   };
+};
 
+//history 
+function addHistory(parentEle) {
+  const historyContainer = getElementById("historyContainer");
+  const input = parentEle.querySelector(".input");
+  const cardTitle = parentEle.querySelector(".card-title");
+  const date = new Date();
+
+  historyContainer.innerHTML += `
+   <div class="bg-base_200 border border-base_content/10 rounded-2xl p-5 md:p-6 lg:p-8">
+        <h2 class="text-lg font-bold text-base_content md:text-xl">${input.value} Taka is Donated for
+          ${cardTitle.innerText}</h2>
+        <p class="text-sm font-light text-base_content_secondary mt-2 md:mt-4 md:text-base">Date : ${date}</p>
+    </div>
+  `;
+
+  input.value = ""; //reset the input value
 };
 
